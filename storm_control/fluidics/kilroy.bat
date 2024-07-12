7c9defb044cde47b278949bee35572d8f434f39d
aaron <7842040+aaronhalpern@users.noreply.github.com>
2024-07-12 09:39:53 -0400

    bat file

--- storm_control/fluidics/kilroy.bat
+++ storm_control/fluidics/kilroy.bat
@@ -1,2 +1,2 @@
-rem Activate virtual environment and start Dave.
-c:\users\hazen\pyenv\hal2\scripts\activate && python kilroy.py kilroy_settings_hbaic1.xml
+call activate hal
+python kilroy.py kilroy_settings_default.xml
